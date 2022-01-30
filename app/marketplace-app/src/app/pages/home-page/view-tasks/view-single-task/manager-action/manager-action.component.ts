462269277c127e1005639dc4de19bf901a8bb975
unknown <58860510+DuArms@users.noreply.github.com>
2022-01-30 02:15:35 +0200

    managers page

--- app/marketplace-app/src/app/pages/home-page/view-tasks/view-single-task/manager-action/manager-action.component.ts
+++ app/marketplace-app/src/app/pages/home-page/view-tasks/view-single-task/manager-action/manager-action.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ContractCallService } from 'src/app/services/contracts-call.service';
+import { ContractsService } from 'src/app/services/contracts.service';
+import { UserService } from 'src/app/services/user.service';
+import { DomainExpertise, UserInfo, USER_STATICS } from 'src/app/types/user-info';
+import { ContractTask } from 'src/app/types/user-types';
 
 @Component({
   selector: 'app-manager-action',
   templateUrl: './manager-action.component.html',
   styleUrls: ['./manager-action.component.scss']
 })
-export class ManagerActionComponent implements OnInit {
+export class ManagerActionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subs: Subscription[] = [];
 
-  ngOnInit(): void {
+  @Input()
+  public task: ContractTask | undefined = undefined;
+  private user: UserInfo = USER_STATICS.EMPTY_USER;
+
+  public applicants: UserInfo[] = [];
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly tx: ContractCallService,
+    private readonly contracts: ContractsService) { }
+
+  public async ngOnInit(): Promise<void> {
+    this.subs.push(this.userService.userObservable().subscribe(data => this.user = data));
+    if (this.task === undefined) {
+      return;
+    }
+
+    if (this.task.applicationsCount > 0) {
+      this.loadApplications();
+    }
   }
 
+  public ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
+  }
+
+  private loadApplications(): void {
+    // const res = this.tx.call(this.contracts.marketplaceContract, "taskApplications", [this.task!.index]);
+    this.applicants = [{
+      ...USER_STATICS.EMPTY_USER,
+      name: "applier1",
+      domainExpertise: DomainExpertise[DomainExpertise.FRONTEND]
+    }, {
+      ...USER_STATICS.EMPTY_USER,
+      name: "applier2",
+      domainExpertise: DomainExpertise[DomainExpertise.BACKEND]
+    }]
+  }
 }
